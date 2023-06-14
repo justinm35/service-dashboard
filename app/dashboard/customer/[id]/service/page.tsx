72b5aceb2fa732461a8ec8dd015603b564b62a08
justinm35 <justin.martin3501@gmail.com>
2023-06-13 21:52:42 -0400

    Added Service Form

--- app/dashboard/customer/[id]/service/page.tsx
+++ app/dashboard/customer/[id]/service/page.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link'
 import { FC } from 'react'
 
 interface ServicesProps {
-  
+  params: {id: string}
 }
 
-const Services: FC<ServicesProps> = ({}) => {
+const Services: FC<ServicesProps> = ({params}) => {
   return (
     <div className='w-full flex flex-col items-center justify-center text-gray-900'>
     <TruckIcon className='w-14 h-14 text-gray-800 mt-32'/>
     <h3 className='font-semibold text-lg mb-4'>No service done yet</h3>
     <p className='opacity-40 mb-5'>Any service calls will show the recors here.</p>
-    <Link href="#" className=' flex items-center max-w-md w-42 py-2 px-4 text-sm font-medium text-center text-white border cursor-pointer bg-orange-400 border-orange-500 rounded-lg hover:bg-orange-500 focus:ring-4 focus:ring-orange-300'>
+    <Link href={`dashboard/customer/${params.id}/service/addservice`} className=' flex items-center max-w-md w-42 py-2 px-4 text-sm font-medium text-center text-white border cursor-pointer bg-orange-400 border-orange-500 rounded-lg hover:bg-orange-500 focus:ring-4 focus:ring-orange-300'>
       <PlusIcon className="w-6 h-6 mr-3"/>Add Service Sheet</Link>
     </div>
   )
